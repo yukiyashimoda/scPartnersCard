91dc0d188133c39bcbfcddaf3e328c04c6ff4bea
apple <apple@appurunoMacBook-Pro.local>
2024-01-20 23:34:36 +0900

    hero re

--- components/Information/InformationModal.tsx
+++ components/Information/InformationModal.tsx
@@ -27,15 +27,15 @@ function InformationModal({partner}:any) {
           <div className='flex'>
            <div>
            <button className="relative inline-flex items-center justify-center w-[52px] sm:w-[42px] h-[52px] sm:h-[42px] p-1 mb-1 me-2 overflow-hidden text-[10px] font-medium text-gray-900 rounded-lg group bg-emerald-700 ">
-  <span className="relative px-5 py-2.5 transition-all ease-in duration-75 text-white rounded-md group-hover:bg-opacity-0 whitespace-nowrap">
-    {modifyCategoryDropdown(partner.categoryDropdown)}
-  </span>
-</button>
+           <span className="relative px-5 py-2.5 transition-all ease-in duration-75 text-white rounded-md group-hover:bg-opacity-0 whitespace-nowrap">
+           {modifyCategoryDropdown(partner.categoryDropdown)}
+          </span>
+          </button>
 
 
            </div>
            <div className='truncate'>
-            <h2 className='text-[16px] font-bold  truncate'>{partner?.companyName}</h2>
+            <h2 className='text-[16px] font-bold  truncate text-black'>{partner?.companyName}</h2>
             <h4 className='text-[12px] text-emerald-600 font-midium mb-2 truncate'>{partner.shopName}</h4>
            </div>
           </div>
@@ -70,7 +70,7 @@ function InformationModal({partner}:any) {
           </div>
           
           <div>
-            <h2 className=' text-[14px] font-light pb-5'>{partner?.introduction}</h2>
+            <h2 className=' text-[14px] font-light pb-5 text-black'>{partner?.introduction}</h2>
           </div>
 
           <div className='flex h-100 mb-5 '>
@@ -82,19 +82,19 @@ function InformationModal({partner}:any) {
             </button>
             </div>
             <div>
-              <h2 className='text-[14px] font-light ml-1'>{partner?.terms}</h2>
+              <h2 className='text-[14px] font-light ml-1 text-gray-600'>{partner?.terms}</h2>
               <h2 className='text-[18px] font-bold pb-0 ml-1 text-emerald-600'>{partner?.benefit}</h2>
             </div>
           </div>
           <div>
-            <h2 className='line-clamp-5 text-[12px] font-light'>{partner?.notice}</h2>
+            <h2 className='line-clamp-5 text-[12px] font-light text-gray-600'>{partner?.notice}</h2>
             </div>
           <div className='flex h-100 mt-4 mb-4 border-t border-b border-gray-300  '>
             <div className='mt-8 mb-8'>
             <img className="h-8 w-8" src="../map-pin-gray.svg" alt="" />
             </div>
             <div>
-              <h2 className='text-[14px] font-light ml-3 mt-5'>〒{partner?.postNumber}</h2>
+              <h2 className='text-[14px] font-light ml-3 mt-5 text-black'>〒{partner?.postNumber}</h2>
               <h2 className='text-[14px] font-light pb-0 ml-3 mb-1 text-black'>{partner?.address}</h2>
             </div>
           </div>
@@ -103,7 +103,7 @@ function InformationModal({partner}:any) {
             <img className="h-8 w-8" src="../time.svg" alt="" />
             </div>
             <div>
-              <h2 className='text-[14px] font-light ml-3 mt-5'>営業時間:{partner?.businessTime}</h2>
+              <h2 className='text-[14px] font-light ml-3 mt-5 text-black'>営業時間:{partner?.businessTime}</h2>
               <h2 className='text-[14px] font-light pb-0 ml-3 mb-5 text-black'>{partner?.holiday}</h2>
             </div>
           </div>
