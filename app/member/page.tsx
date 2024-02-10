d9e91628b0ca5047e8be80c15e78041bf2c294fc
apple <apple@appurunoMacBook-Pro.local>
2024-02-11 05:57:51 +0900

    png

---
+++ app/member/page.tsx
@@ -0,0 +1,21 @@
+"use client"
+import Image from 'next/image';
+
+
+
+export default function Home() {
+
+  return (
+    <div className=''>
+      <img src='SC_01.png' alt="Special Club" />
+      <img src='SC_02.png' alt="Special Club" />
+      <img src='SC_03.png' alt="Special Club" />
+      <img src='SC_04.png' alt="Special Club" />
+      <img src='SC_05.png' alt="Special Club" />
+      <img src='SC_06.png' alt="Special Club" />
+      <img src='SC_07.png' alt="Special Club" />
+      <img src='SC_08.png' alt="Special Club" />
+      <img src='SC_09.png' alt="Special Club" />
+    </div>
+  )
+}
