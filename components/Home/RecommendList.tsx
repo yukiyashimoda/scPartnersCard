28bc40d0f8e65de5f1be92df5c3c7d5b19ee45d1
apple <apple@appurunoMacBook-Pro.local>
2024-02-13 00:58:55 +0900

    recommend

--- components/Home/RecommendList.tsx
+++ components/Home/RecommendList.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
 import RecommendCard from './RecommendCard';
-import InformationModal from '../Information/InformationModal';
 
 interface Partner {
-  // パートナーカードのデータ構造に合わせたインターフェースを定義してください
-  // 例: id: number;
-  //     name: string;
-  //     category: string;
-  //     他に必要なプロパティがあれば追加してください
+  id: number;
+  name: string;
+  category: string;
+  recommend: boolean; 
 }
-
       
 interface RecommendListProps {
   partnersList: Partner[];
 }
 
 const RecommendList: React.FC<RecommendListProps> = ({ partnersList }) => {
+  const recommendedPartners = partnersList.filter(partner => partner.recommend);
+
   return (
-    
     <div className='flex overflow-x-auto p-5'>
-      {partnersList.map((partner, index) => (
+      {recommendedPartners.map((partner, index) => (
         <div key={index} className='flex-none mr-3' style={{ width: '45%', height: '400px'}}>
           <RecommendCard partner={partner} />
         </div>
