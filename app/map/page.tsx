0edb854386cc6db47e970c8434d08c0284aa9f87
apple <apple@appurunoMacBook-Pro.local>
2024-02-17 04:28:38 +0900

    map

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,13 +1,15 @@
-"use client"
 import React from 'react';
-import Map from '@/components/map';
-
-
 
 export default function Home() {
   return (
-    <>
-      <Map />
-    </>
+    <div style={{ height: '78vh' }}>
+      <iframe
+        src="https://www.google.com/maps/d/u/1/embed?mid=1Ypgk8DYBHrNg3YuBt__MZdLwzLRI2XI&ehbc=2E312F&noprof=1"
+        width="100%"
+        height="100%"
+        style={{ border: 0 }}
+        allowFullScreen
+      ></iframe>
+    </div>
   );
 }
